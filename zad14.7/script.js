ad2499b0109272d67bdb59b55c3374c34dabc8bf
sa1amander83 <virom8@yandex.ru>
2020-11-22 22:00:30 +0300

    Домашка 14.7

--- zad14.7/script.js
+++ zad14.7/script.js
@@ -1,36 +1,163 @@
-
-  let inputid = +document.getElementById("#inputid");
-  const btn = document.querySelector('#getbtn');
 function pageLoaded() {
 
- 
+  const btn = document.querySelector("#button");
 
-   btn.addEventListener("click", check);
-  function check(){
-    
-    
-    // Делаем запрос за данными
-    fetch(`https://jsonplaceholder.typicode.com/users/${inputid}/todos`)
-      .then((response) => {
-        // Объект ответа на запрос
-        console.log('response', response);
-        // Превращаем объект в JSON. Мы не можем его сразу прочитать,
-        // надо отдать в следующий then
-        const result = response.json();
-        console.log('result', result);
-        return result;
+  const output = document.querySelector("#output");
+
+
+
+   function sendRequest() {
+    const inputid = +document.querySelector("#page").value;
+   fetch(`https://jsonplaceholder.typicode.com/users/${+inputid}/todos`)
+      .then(response => {
+             return response.json();
+
       })
-      .then((data) => {
-        // Объект результата в формате JSON
+      .then(data => {
         console.log(data);
-      })
-      .catch(() => { console.log('Пользователь с указанным id не найден'); });
+       formatOutput(data);
+      
+          });
+          // .catch(() => {
+          //   console.log('Пользователь с указанным id не найден'); });
+          }
+   
+
+  function formatOutput(data) {
+
+
  
+
+          let todoAll = '';
+     data.forEach(item => {
+
+ const todoid=item.id;
+  const userid=item.userId;
+  const title=item.title;
+  const complete=item.completed;
+
+if (complete==false){
+       const todoUncomplete = `
+          <ul>{
+    userid: ${item.userId},
+    id: ${item.id},
+    title: ${item.title},
+    completed: ${item.completed}
+}</ul> `;
+
+       todoAll += todoUncomplete;
+   }else {
+    const todoComplete = `
+    <ul> <s>{
+userid: ${item.userId},
+id: ${item.id},
+title: ${item.title},
+completed: ${item.completed}
+}</s></ul> `;
+todoAll+=todoComplete;
+  }
+  });
+
+
+     output.innerHTML = todoAll;
+    
+  }
   
-
-
+ btn.addEventListener("click", sendRequest);
 }
 
-}
-  
 document.addEventListener("DOMContentLoaded", pageLoaded);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   function pageLoaded() {
+
+//     const btn = document.querySelector('#getbtn');
+//  function check(){
+
+//     let inputid = +document.querySelector("#inputid").value;
+//    // Делаем запрос за данными 
+ 
+//    const url=`https://jsonplaceholder.typicode.com/users/3/todos`;
+   
+  
+
+
+//     fetch(`https://jsonplaceholder.typicode.com/users/${inputid}/todos`)
+//         .then(response => {
+//             return response.json();
+//     // Объект ответа на запрос
+//       //  console.log('response', response);
+     
+//       //  const result = response.json();
+//       //  console.log('result', result);
+//       //  return result;
+//      })
+//      .then(data => {
+//        // Объект результата в формате JSON
+// console.log(data);
+// writeoutput(data);
+//            })
+//       .catch(() => {
+//    console.log('Пользователь с указанным id не найден'); });
+  
+//  }
+   
+// btn.addEventListener("click", check);
+// }
+
+// function writeoutput(data) {
+//   const todoData = JSON.parse(data);  
+  
+//   let output = document.getElementById("#output");
+  
+//           let todos = '';
+//      data.forEach(item => {
+//  const todoid=todoData.id;
+//   const userid=todoData.userid;
+//   const title=todoData.title;
+//   const complete=todoData.completed;
+//        const todoElement = `
+//           <ul>{
+//     userid: ${todoData.userid},
+//     id: ${todoData.id},
+//     title: ${todoData.title},
+//     completed: ${todoData.complete}
+// }</ul>
+            
+//      `;
+//        todos += todoElement;
+//       });
+ 
+//      output.innerHTML = todos;
+   
+   
+  
+// }
+  
+// document.addEventListener("DOMContentLoaded", pageLoaded);
